b7211a71d1bfc544d75509b82e60f6d3e1f8ef36
AdilBikeev <bikeev1999@mail.ru>
2022-02-27 00:07:21 +0300

    Add lib for EventBus

---
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Stock.BuildingBlocks.EventBus.Abstractions
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Events;
+
+namespace Stock.BuildingBlocks.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+
+}
