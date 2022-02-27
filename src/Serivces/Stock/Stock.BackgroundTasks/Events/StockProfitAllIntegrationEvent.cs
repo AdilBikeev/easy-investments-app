a58e8eeae4193d708ef848cdd2d97fa6d4633cf2
AdilBikeev <bikeev1999@mail.ru>
2022-02-27 03:10:18 +0300

    Add for example BackgroundTask project

---
+++ src/Serivces/Stock/Stock.BackgroundTasks/Events/StockProfitAllIntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿using Stock.BuildingBlocks.EventBus.Events;
+
+namespace Stock.BackgroundTasks.Events
+{
+    /// <summary>
+    /// Событие для получения Id строки таблицы stock.StockProfit.
+    /// </summary>
+    public record StockProfitIntegrationEvent : IntegrationEvent
+    {
+        public int StockProfitId { get; }
+
+        public StockProfitIntegrationEvent(int stockProfitId) =>
+            StockProfitId = stockProfitId;
+    }
+}
---
+++ src/Serivces/Stock/Stock.BackgroundTasks/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Reflection;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Autofac.Extensions.DependencyInjection;
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+
+global using Serilog;
